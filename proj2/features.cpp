2a320e6841b48b502a1f6319dc64d068e06c4d48
Yueyang Wu <yueyangwu983@gmail.com>
2022-02-07 23:52:00 -0800

    add readme and code from project1

--- proj2/features.cpp
+++ proj2/features.cpp
@@ -43,7 +43,7 @@ vector<float> histogram(Mat &image) {
             feature.at<int>(b, g, r)++;
         }
     }
-    
+
     // convert the 3D histogram into a 1D vector
     return matToVector(feature);
 }
@@ -76,4 +76,141 @@ vector<float> matToVector(Mat &m) {
     Mat flat = m.reshape(1, m.total() * m.channels());
     flat.convertTo(flat, CV_32F);
     return m.isContinuous() ? flat : flat.clone();
+}
+
+
+// codes from project1
+int sobelX3x3(const Mat &src, Mat &dst) {
+    // build the filters
+    int horizontalFilterArray[] = {-1, 0, 1};
+    int verticalFilterArray[] = {1, 2, 1};
+    Mat horizontalFilter(1, 3, CV_16SC1, horizontalFilterArray);
+    Mat verticalFilter(1, 3, CV_8UC1, verticalFilterArray);
+
+    // both src and dst of filter1xN and filterNx1 should be type CV_32FC3
+    Mat src32FC3;
+    src.convertTo(src32FC3, CV_32FC3);
+    Mat temp(src.size(), CV_32FC3);
+    Mat dst32FC3(src.size(), CV_32FC3);
+
+    // apply horizontal filter
+    filter1xN(src32FC3, temp, horizontalFilter, 1);
+    // apply vertical filter
+    filterNx1(temp, dst32FC3, verticalFilter, 4);
+
+    // convert the dst type to CV_16SC3 otherwise there be problem with the pointer
+    dst32FC3.convertTo(dst, CV_16SC3);
+
+    return 0;
+}
+
+int sobelY3x3(const Mat &src, Mat &dst) {
+    // build the filters
+    int horizontalFilterArray[] = {1, 2, 1};
+    int verticalFilterArray[] = {1, 0, -1};
+    Mat horizontalFilter(1, 3, CV_16SC1, horizontalFilterArray);
+    Mat verticalFilter(1, 3, CV_8UC1, verticalFilterArray);
+
+    // both src and dst of filter1xN and filterNx1 should be type CV_32FC3
+    Mat src32FC3;
+    src.convertTo(src32FC3, CV_32FC3);
+    Mat temp(src.size(), CV_32FC3);
+    Mat dst32FC3(src.size(), CV_32FC3);
+
+    // apply horizontal filter
+    filter1xN(src32FC3, temp, horizontalFilter, 4);
+    // apply vertical filter
+    filterNx1(temp, dst32FC3, verticalFilter, 1);
+
+    // convert the dst type to CV_16SC3 otherwise there be problem with the pointer
+    dst32FC3.convertTo(dst, CV_16SC3);
+
+    return 0;
+}
+
+int magnitude(const Mat &sx, const Mat &sy, Mat &dst) {
+    // to use cv::sqrt, the input and output type need to be CV_32FC3
+    Mat sx32FC3, sy32FC3, dst32FC3;
+    sx.convertTo(sx32FC3, CV_32FC3);
+    sy.convertTo(sy32FC3, CV_32FC3);
+
+    sqrt(sx32FC3.mul(sx32FC3) + sy32FC3.mul(sy32FC3), dst32FC3);
+
+    // convert the dst type back to CV_16SC3
+    dst32FC3.convertTo(dst, CV_16SC3);
+
+    return 0;
+}
+
+/*
+ * filter1xN and filterNx1 are two helper functions
+ * Since the filters will contain both positive and negative integers and
+ * there will be different types of src and dst from the functions using these helper functions,
+ * the two helper functions will only input/output CV_32FC3 type, which is a more general type
+ * the functions using these helpers should convert the src and dst types according to the actual needs
+ */
+int filter1xN(const Mat &src, Mat &dst, Mat &filter, int normalizer) {
+    // validate input
+    CV_Assert(filter.channels() == 1);
+    CV_Assert(filter.rows == 1); // assume filter is 1d array
+    CV_Assert(dst.size() == src.size());
+    CV_Assert(src.type() == CV_32FC3 && dst.type() == CV_32FC3);
+
+    // apply the 1xN filter and normalize the result
+    // treat pixels off the edge as having asymmetric reflection over that edge
+    int radius = filter.cols / 2;
+    for (int i = 0; i < src.rows; i++) {
+        for (int j = 0; j < src.cols; j++) {
+            int blue = 0, green = 0, red = 0;
+            for (int k = -radius; k <= radius; k++) {
+                if (j + k < 0 || j + k >= src.cols) {
+                    blue += filter.at<int>(radius + k) * src.at<Vec3f>(i, j - k)[0];
+                    green += filter.at<int>(radius + k) * src.at<Vec3f>(i, j - k)[1];
+                    red += filter.at<int>(radius + k) * src.at<Vec3f>(i, j - k)[2];
+                } else {
+                    blue += filter.at<int>(radius + k) * src.at<Vec3f>(i, j + k)[0];
+                    green += filter.at<int>(radius + k) * src.at<Vec3f>(i, j + k)[1];
+                    red += filter.at<int>(radius + k) * src.at<Vec3f>(i, j + k)[2];
+                }
+            }
+            dst.at<Vec3f>(i, j)[0] = blue / normalizer;
+            dst.at<Vec3f>(i, j)[1] = green / normalizer;
+            dst.at<Vec3f>(i, j)[2] = red / normalizer;
+        }
+    }
+
+    return 0;
+}
+
+int filterNx1(const Mat &src, Mat &dst, Mat &filter, int normalizer) {
+    // validate input
+    CV_Assert(filter.channels() == 1);
+    CV_Assert(filter.rows == 1); // assume filter is 1d array
+    CV_Assert(dst.size() == src.size());
+    CV_Assert(src.type() == CV_32FC3 && dst.type() == CV_32FC3);
+
+    // apply the Nx1 filter and normalize the result
+    // treat pixels off the edge as having asymmetric reflection over that edge
+    int radius = filter.cols / 2;
+    for (int i = 0; i < src.rows; i++) {
+        for (int j = 0; j < src.cols; j++) {
+            int blue = 0, green = 0, red = 0;
+            for (int k = -radius; k <= radius; k++) {
+                if (i + k < 0 || i + k > src.rows) {
+                    blue += filter.at<int>(radius + k) * src.at<Vec3f>(i - k, j)[0];
+                    green += filter.at<int>(radius + k) * src.at<Vec3f>(i - k, j)[1];
+                    red += filter.at<int>(radius + k) * src.at<Vec3f>(i - k, j)[2];
+                } else {
+                    blue += filter.at<int>(radius + k) * src.at<Vec3f>(i + k, j)[0];
+                    green += filter.at<int>(radius + k) * src.at<Vec3f>(i + k, j)[1];
+                    red += filter.at<int>(radius + k) * src.at<Vec3f>(i + k, j)[2];
+                }
+            }
+            dst.at<Vec3f>(i, j)[0] = blue / normalizer;
+            dst.at<Vec3f>(i, j)[1] = green / normalizer;
+            dst.at<Vec3f>(i, j)[2] = red / normalizer;
+        }
+    }
+
+    return 0;
 }
---
+++ proj2/statics/readme.txt
@@ -0,0 +1,19 @@
+// CS5330 Project 1
+// Yueyang Wu
+
+- Links/URLs to any videos you created and want to submit as part of your report.
+    N/A
+
+- The URL for your wiki report for this project.
+    https://wiki.khoury.northeastern.edu/display/~yueyangwu/CS5330_Project2
+
+- What operating system and IDE you used to run and compile your code.
+    MacOS 12.1 (Monterey)
+    CLion, CMake, and terminal
+
+- Instructions for running your executables.
+
+- Instructions for testing any extensions you completed.
+
+- Whether you are using any time travel days and how many.
+    No.
