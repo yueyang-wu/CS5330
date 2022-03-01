90ec5c1809a1c5e2366f03d55a595635980c6927
Yueyang Wu <yueyangwu983@gmail.com>
2022-03-01 00:23:46 -0800

    display regions with same color

--- proj3/objectRecognizer.cpp
+++ proj3/objectRecognizer.cpp
@@ -24,7 +24,7 @@ int main() {
     namedWindow("Original Video", 1);
     namedWindow("Processed Video", 1);
 //    namedWindow("Test Video", 1);
-    Mat frame, processedFrame;
+    Mat frame, thresholdFrame, processedFrame;
 
     while (true) {
         *capdev >> frame; // get a new frame from the camera, treat as a stream
@@ -35,10 +35,30 @@ int main() {
         imshow("Original Video", frame);
 
         // threshold the image
-        processedFrame = threshold(frame);
+        thresholdFrame = threshold(frame);
         // clean up the image
         const Mat kernel = getStructuringElement(MORPH_CROSS, Size(25, 25));
-        morphologyEx(processedFrame, processedFrame, MORPH_CLOSE, kernel);
+        morphologyEx(thresholdFrame, thresholdFrame, MORPH_CLOSE, kernel);
+        // get the region
+
+//        Mat labelImage(img.size(), CV_32S);
+        Mat stats, centroids;
+        int nLabels = connectedComponentsWithStats(thresholdFrame, thresholdFrame, stats, centroids);
+        vector<Vec3b> colors(nLabels);
+
+        colors[0] = Vec3b(0, 0, 0);//background
+        for(int label = 1; label < nLabels; ++label){
+            colors[label] = Vec3b(100, 150, 200);
+        }
+        processedFrame.create(thresholdFrame.size(), CV_8UC3);
+        for(int r = 0; r < processedFrame.rows; ++r){
+            for(int c = 0; c < processedFrame.cols; ++c){
+                int label = thresholdFrame.at<int>(r, c);
+                Vec3b &pixel = processedFrame.at<Vec3b>(r, c);
+                pixel = colors[label];
+            }
+        }
+
         imshow("Processed Video", processedFrame);
 
         // see if there is a waiting keystroke
